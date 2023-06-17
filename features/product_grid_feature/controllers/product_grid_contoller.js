e0b30eba7113a6a1a0e444ae0ad8eb0e9d44f750
Franco Agustin Cola Ibañez <acola@clarika.com.ar>
2023-06-17 18:03:14 -0300

    Refactor header

--- features/product_grid_feature/controllers/product_grid_contoller.js
+++ features/product_grid_feature/controllers/product_grid_contoller.js
@@ -2,7 +2,7 @@ const leftList = document.getElementById('left_column');
 const rightList = document.getElementById('right_column');
 
 botonAux.addEventListener('click', () => {
-  const newElement = document.createElement('li')
+  const newElement = document.createElement('div')
   newElement.textContent = 'Funciona'
   newElement.className = 'product_card'
   leftList.append(newElement)
