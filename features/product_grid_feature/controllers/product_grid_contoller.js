11b59498b04554fe387f2b8c8cbbaa6c0545772f
Franco Agustin Cola Ibañez <acola@clarika.com.ar>
2023-06-17 23:40:20 -0300

    Conecct web page to a backend

--- features/product_grid_feature/controllers/product_grid_contoller.js
+++ features/product_grid_feature/controllers/product_grid_contoller.js
@@ -1,46 +1,19 @@
-
 const productGrid = document.getElementById('product_grid')
 
 window.addEventListener('load', async () => {
-  const productList = [
-    {
-      "product_name" : "Carbones",
-      "image_url" : "https://picsum.photos/200"
-    },
-    {
-      "product_name" : "Plaquetas",
-      "image_url" : "https://picsum.photos/201"
-    },
-    {
-      "product_name" : "Buges",
-      "image_url" : "https://picsum.photos/202"
-    },
-    {
-      "product_name" : "Cables",
-      "image_url" : "https://picsum.photos/203"
-    },
-    {
-      "product_name" : "Bobinas",
-      "image_url" : "https://picsum.photos/204"
-    },
-    {
-      "product_name" : "Horquillas",
-      "image_url" : "https://picsum.photos/205"
-    },
-    {
-      "product_name" : "Fusibles",
-      "image_url" : "https://picsum.photos/206"
-    },
-  ]
+  console.log(productGrid)
+  const response = await axios.get('http://localhost:8000/api/products');
+  const productList = response.data['products']
+
 
   for (const product of productList) {
     const newElement = document.createElement('div');
     newElement.className = 'product_card';
     const imageElement = document.createElement('img');
     imageElement.className = 'product_image'
-    imageElement.src = product["image_url"];
+    imageElement.src = product["image_url"] ?? 'https://picsum.photos/200';
     const textElement = document.createElement('h2')
-    textElement.textContent = product["product_name"]
+    textElement.textContent = product["name"]
 
 
     newElement.appendChild(imageElement);
