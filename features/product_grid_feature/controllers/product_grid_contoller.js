f4ec679ab0136aad21772717a1ed19eb2231e396
Franco Agustin Cola Ibañez <acola@clarika.com.ar>
2023-06-19 23:04:12 -0300

    add button

--- features/product_grid_feature/controllers/product_grid_contoller.js
+++ features/product_grid_feature/controllers/product_grid_contoller.js
@@ -7,19 +7,33 @@ window.addEventListener('load', async () => {
 
 
   for (const product of productList) {
-    const newElement = document.createElement('div');
-    newElement.className = 'product_card';
-    const imageElement = document.createElement('img');
-    imageElement.className = 'product_image'
+    const newElement = createElementWith('div', 'product_card')
+
+    const imageElement = createElementWith('img', 'product_image')
     imageElement.src = product["image_url"] ?? 'https://picsum.photos/200';
-    const textElement = document.createElement('h2')
-    textElement.textContent = product["name"]
+    const textElement = createElementWith('h2', null, product['name'])
+    const addButton = document.createElement('button', 'add_button');
+    addButton.style.borderRadius = '100%'
+    addButton.style.backgroundColor = 'black'
+    const addIcon = createElementWith('i', 'material-icons', 'add',)
+    addButton.style.color = 'white'
 
 
+    addButton.appendChild(addIcon);
     newElement.appendChild(imageElement);
-    productGrid.appendChild(newElement);
-    newElement.appendChild(textElement)
-  }
+    newElement.appendChild(textElement);
+    newElement.appendChild(addButton);
 
+    productGrid.appendChild(newElement);
+  }
 })
+
+const createElementWith = (elementType, className, textContent) => {
+  const newElement = document.createElement(elementType)
+  newElement.className = className;
+  if (textContent != null) {
+    newElement.textContent = textContent
+  }
+  return newElement
+}
 
