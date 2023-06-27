d728b7a4844fe7e966115714f61b58e42d57d12e
Franco Agustin Cola Ibañez <acola@clarika.com.ar>
2023-06-26 22:09:02 -0300

    Functional searchBar

--- features/product_grid_feature/controllers/product_grid_contoller.js
+++ features/product_grid_feature/controllers/product_grid_contoller.js
@@ -2,35 +2,13 @@ const productGrid = document.getElementById('product_grid')
 const productsInCartMap = new Map()
 const productCounter = document.getElementById('cart_counter')
 const cartButton = document.getElementById('cart_button')
+const searchBar = document.getElementById('search_bar')
+const searchButton = document.getElementById('search_button')
 
-window.addEventListener('load', async () => {
-  const response = await axios.get('http://localhost:8000/api/products');
-  const productList = response.data['products']
+window.addEventListener('load', async () => getProducts())
 
-
-  for (const product of productList) {
-    const newElement = createElementWith('div', 'product_card')
-
-    const imageElement = createElementWith('img', 'product_image')
-    imageElement.src = product["image_url"] ?? 'https://picsum.photos/200';
-    const textElement = createElementWith('h2', null, product['name'])
-    const addButton = document.createElement('button', 'add_button');
-    addButton.style.borderRadius = '100%'
-    addButton.style.backgroundColor = 'black'
-    const addIcon = createElementWith('i', 'material-icons', 'add',)
-    addButton.style.color = 'white'
-
-
-    addButton.appendChild(addIcon);
-    newElement.appendChild(imageElement);
-    newElement.appendChild(textElement);
-    newElement.appendChild(addButton);
-
-    productGrid.appendChild(newElement);
-    addButton.addEventListener('click', () => {
-      addProductToCart(product)
-    })
-  }
+searchButton.addEventListener('click', async () => {
+  await getProducts(searchBar.value);
 })
 
 const createElementWith = (elementType, className, textContent) => {
@@ -64,3 +42,38 @@ const getTotalProducts = () => {
   } 
   return counter
 }
+
+const getProducts = async (searchField) => {
+  productGrid.innerHTML = '';
+  let url = 'http://localhost:8000/api/products'
+  if(searchField){
+    url += `?filterQuery=${searchField}`
+  }
+  const response = await axios.get(url);
+  const productList = response.data['products']
+
+
+  for (const product of productList) {
+    const newElement = createElementWith('div', 'product_card')
+
+    const imageElement = createElementWith('img', 'product_image')
+    imageElement.src = product["image_url"] ?? 'https://picsum.photos/200';
+    const textElement = createElementWith('h2', null, product['name'])
+    const addButton = document.createElement('button', 'add_button');
+    addButton.style.borderRadius = '100%'
+    addButton.style.backgroundColor = 'black'
+    const addIcon = createElementWith('i', 'material-icons', 'add',)
+    addButton.style.color = 'white'
+
+
+    addButton.appendChild(addIcon);
+    newElement.appendChild(imageElement);
+    newElement.appendChild(textElement);
+    newElement.appendChild(addButton);
+
+    productGrid.appendChild(newElement);
+    addButton.addEventListener('click', () => {
+      addProductToCart(product)
+    })
+  }
+} 
