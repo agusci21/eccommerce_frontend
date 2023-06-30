81419412d03bcedf5c7fb03a2c6c898238bc60fb
Franco Agustin Cola Ibañez <acola@clarika.com.ar>
2023-06-30 11:50:21 -0300

    Add product summary

--- features/product_summary/controllers/product_summary_controller.js
+++ features/product_summary/controllers/product_summary_controller.js
@@ -1,10 +1,12 @@
 
+const productContainer = document.getElementById('product_container');
 let productsInCartList = []
 
 window.addEventListener('load', async () => {
-  productsInCartList = getProductsFromSessionStorage();
+  productsInCartList = getFakeProducts()//getProductsFromSessionStorage();
   console.log(productsInCartList)
 });
+
 
 const getProductsFromSessionStorage = () => {
     const decodedProductsInCart = JSON.parse(sessionStorage.getItem('products_in_cart'));
@@ -13,4 +15,32 @@ const getProductsFromSessionStorage = () => {
     }
     return [];
   }
-  
+
+  const getFakeProducts = () => [
+    new Product(
+      '1',
+      'Horquilla',
+      'Horquillas de renault 12',
+      20,
+      1300,
+      1,
+    ),
+    new Product(
+      '2',
+      'Arandelas',
+      'Arandelas metalicas',
+      300,
+      200,
+      20,
+    ),
+    new Product(
+      '3',
+      'Bugias',
+      '987 1x',
+      12,
+      50,
+      2,
+    ),
+    
+  ]
+
